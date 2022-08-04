1ab0f6b3a083fee2125823d2c7ff3718a52c46dc
quaintrelle7 <sharayushendre7@gmail.com>
2022-08-04 16:49:05 +0530

    Updates Functions.sol

---
+++ 02_Day_Types/References.sol
@@ -0,0 +1,7 @@
+//  References Types 
+
+/* Memory: Whose lifetime is limited to an external fn call
+   Storage: Where the state vars are stores
+   Lifetime is limited to the lifetime of a contract
+   Calldata: Special Data locations that contains the fn arguments
+ */
