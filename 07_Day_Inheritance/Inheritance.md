6989a7519065ef7da6a99c3bc8873fe8f84eee1e
quaintrelle7 <sharayushendre7@gmail.com>
2022-08-21 23:39:12 +0530

    Added Inheritance

---
+++ 07_Day_Inheritance/Inheritance.md
@@ -0,0 +1,8 @@
+# Inheritance
+
+- Supports Polymorphism
+- Multiple Inhertance is Possible
+- virtual keyword -- function can change its behaviour in derived class
+- To override a function use override and virtual keyword
+- Possible to call a specific overridden function.
+- Specifying just 'override' and not 'virtual' doesn't change the behaviour of the function.
--- 07_Day_Inheritance/Inheritance.sol
+++ 07_Day_Inheritance/Inheritance.sol
@@ -16,5 +16,110 @@ contract Destructible is Owned{
 // The keyword `virtual` means that the function can change
 // its behaviour in derived classes ("overriding").
 
+    function destroy() virtual public{
+        if(msg.sender==owner) selfdestructor(owner);
+    }
 
 }
+
+//abstract function just as an interface
+
+abstract contract Config{
+    function lookup(uint id) public virtual returns(address adr);
+}
+
+abstract contract NameReg{
+    function register(bytes32 name) public virtual;
+    function unregister()public virtual;
+}
+
+//Multiple inheritance case
+
+contract Named is Owned, Destructible{
+    constructor(bytes32 name){
+        Config config = Config(0x);
+        NameReg(config.lookup(1)).register(name);
+    }
+
+}
+
+//function can be overriden by another function with same name and 
+//i/o params.
+//use override keyword and virtual keyword to override.
+
+function destroy() public virtual override{
+    if(msg.sender == owner){
+        Config config = Config(0x);
+        NameReg(config.lookup(1)).unregister();
+        //It's still possible to call a specific overridden function
+        Destructible.destroy();
+    }
+}
+
+//If a constructor takes an argument, it needs to be
+//provided in the header or modifier-invocation-style at
+//the constructor of the derived contract
+
+//See for named
+
+contract PriceFeed is Owned, Destructible, Named("Goldfeed){
+    function updateInfo(uint newInfo) public{
+        if(msg.sender == owner) info - newInfo;
+
+        //Here, we only specify 'override' and not 'virtual'
+        // This means that contracts deriving from `PriceFeed`
+    // cannot change the behaviour of `destroy` anymore.
+
+    function destroy() public override(Destructible, Named){Name.destroy();}
+    function get() public view returns(uint r){return info;}
+
+    uint info;
+
+    }
+}
+
+
+//Summary
+
+ontract owned {
+    constructor() { owner = payable(msg.sender); }
+    address payable owner;
+}
+
+contract Destructible is owned {
+    function destroy() public virtual {
+        if (msg.sender == owner) selfdestruct(owner);
+    }
+}
+
+contract Base1 is Destructible {
+    function destroy() public virtual override { /* do cleanup 1 */ Destructible.destroy(); }
+}
+
+contract Base2 is Destructible {
+    function destroy() public virtual override { /* do cleanup 2 */ Destructible.destroy(); }
+}
+
+contract Final is Base1, Base2 {
+    function destroy() public override(Base1, Base2) { Base2.destroy(); }
+}
+
+// A call to Final.destroy() will call Base2.destroy because we specify it explicitly in the final override, 
+// but this function will bypass Base1.destroy. The way around this is to use super
+
+contract Base1 is Destructible {
+    function destroy() public virtual override { /* do cleanup 1 */ super.destroy(); }
+}
+
+
+contract Base2 is Destructible {
+    function destroy() public virtual override { /* do cleanup 2 */ super.destroy(); }
+}
+
+contract Final is Base1, Base2 {
+    function destroy() public override(Base1, Base2) { super.destroy(); }
+}
+
+Final inheritance graph for above contract is:
+Base2, Base1, Destructible, owned
+
